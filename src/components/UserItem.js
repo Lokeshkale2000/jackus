7a19dd71b6df5197552de118df1610efc640e4af
Lokeshkale2000 <lokesh.21910292@viit.ac.in>
2025-01-30 19:32:20 +0530

    submition

---
+++ src/components/UserItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const UserItem = ({ user, onDelete, onEdit }) => {
+  return (
+    <tr>
+      <td>{user.id}</td>
+      <td>{user.name}</td>
+      <td>{user.email}</td>
+      <td>{user.company.name}</td>
+      <td>
+        <button className="edit-btn" onClick={() => onEdit(user)}>
+          Edit
+        </button>
+        <button className="delete-btn" onClick={() => onDelete(user.id)}>
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default UserItem;
